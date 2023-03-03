fed0715e71a654f54755e93c0b543f6cf694e5a2
arti1793 <arti1793@gmail.com>
2023-03-03 22:39:06 +0300

    added diskust tree builder with size calc

---
+++ api/src/lib.rs
@@ -0,0 +1,86 @@
+use file_size::fit_4;
+use std::{
+    fs::read_dir,
+    io::{self, Result},
+    path::Path,
+};
+
+pub struct Node {}
+
+#[derive(Debug)]
+pub struct Diskust {
+    is_file: bool,
+    is_dir: bool,
+    size: u64,
+    nodes: Box<Option<Vec<Diskust>>>,
+}
+
+impl Diskust {
+    fn new(path: &Path) -> Result<Diskust> {
+        let meta = path.metadata();
+        if let Result::Ok(meta) = meta {
+            if meta.is_symlink() {
+                return Result::Err(io::Error::new(io::ErrorKind::NotFound, "skip symlink"));
+            }
+            if meta.is_file() {
+                // println!("file {:?}", path);
+                println!("file {} {}", path.to_str().unwrap(), fit_4(meta.len()));
+                return Ok(Diskust {
+                    is_dir: meta.is_dir(),
+                    is_file: meta.is_file(),
+                    nodes: Box::new(None),
+                    size: meta.len(),
+                });
+            }
+            if meta.is_dir() {
+                // println!("dir {:?}", path.to_str().unwrap());
+                let mut nodes: Vec<Diskust> = vec![];
+                for entry in read_dir(path)? {
+                    if let Result::Ok(entry) = entry {
+                        let node = Self::new(&entry.path());
+                        if let Result::Ok(node) = node {
+                            nodes.push(node);
+                        } else {
+
+                            // println!("skip dir {:?}", &entry.path().to_str().unwrap());
+                        }
+                    } else {
+                        // println!("skip dir {:?}", &entry?.path().to_str().unwrap());
+                    }
+                }
+                let size = nodes.iter().map(|Diskust { size, .. }| size).sum();
+                let dir_node = Ok(Diskust {
+                    is_dir: meta.is_dir(),
+                    is_file: meta.is_file(),
+                    nodes: Box::new(Some(nodes)),
+                    size,
+                });
+                println!("dir {} {} MB", path.to_str().unwrap(), fit_4(size));
+
+                return dir_node;
+            }
+
+            return Result::Err(io::Error::new(io::ErrorKind::NotFound, "no way"));
+        } else {
+            return Result::Err(meta.err().unwrap());
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::ErrorKind;
+
+    use super::*;
+
+    #[test]
+    fn empty() {
+        assert_eq!(
+            Diskust::new(Path::new(&"./no_such_directory".to_string()))
+                .err()
+                .unwrap()
+                .kind(),
+            ErrorKind::NotFound
+        );
+    }
+}
